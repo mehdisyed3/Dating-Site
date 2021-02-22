e9ea1c8797be1b4ca2a908f78c29df891b7b8562
Syed Mehdi <mehdisyed3@gmail.com>
2021-02-22 18:49:02 -0500

    Header.js and .css

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Header.css'
 import IconButton from '@material-ui/core/IconButton'
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import PersonIcon from '@material-ui/icons/Person'
+import ForumIcon from '@material-ui/icons/Forum'
 
 function Header() {
   return (
@@ -10,9 +12,11 @@ function Header() {
         <PersonIcon fontSize='large' className='header__icon' />
       </IconButton>
 
-      <img  className='header__logo' src='https://images.all-free-download.com/images/graphiclarge/cupid_6822703.jpg' alt='' />
+      <img className='header__logo' src='https://www.pngkit.com/png/full/121-1218627_healthcare-icon-business-icon-png-red.png' alt='' />
 
-
+      <IconButton>
+        <ForumIcon fontSize='large' className='header__icon' />
+      </IconButton>
     </div>
   )
 }
