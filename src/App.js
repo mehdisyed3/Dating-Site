34e93e0235dc4e3817f84a7581a2ff8a5474a1dd
Syed Mehdi <mehdisyed3@gmail.com>
2021-02-23 18:24:56 -0500

    minor change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Cards from './Cards/Cards';
 import Header from './Header/Header'
 import SwipeButtons from './SwipeButtons/SwipeButtons';
 
+
+//1:23:18
 function App() {
   return (
     <div className="app">
